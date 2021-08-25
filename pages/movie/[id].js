241e5e4265cc1fce5e236aabc5a2d350f9af12a0
k-cicek <kevserrcicek@gmail.com>
2021-08-26 00:03:38 +0300

    Movie Update

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -3,10 +3,14 @@ import Head from "next/head";
 import Header from "../../components/Header";
 import Hero from "../../components/Hero";
 import Image from "next/image";
+import { useState } from "react";
+import { PlusIcon } from "@heroicons/react/solid";
+
 function Movie({ result }) {
+  console.log(result);
   const [session] = useSession();
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
-
+  const [showPlayer, setShowPlayer] = useState(false);
 
   return (
     <div>
@@ -18,14 +22,75 @@ function Movie({ result }) {
       {!session ? (
         <Hero />
       ) : (
-        <section>
-          <div>
+        <section className="relative z-50">
+          <div className="relative min-h-[calc(100vh-72px)]">
             <Image
               src={
                 `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
                 `${BASE_URL}${result.poster_path}`
               }
+              layout="fill"
+              objectFit="cover"
             />
+          </div>
+          <div className="absolute inset-y-28 md:inset-y-auto md:bottom-10 inset-x-4 md:inset-x-12 space-y-6 z-50">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">
+              {result.title || result.original_name}
+            </h1>
+            <div className="flex items-center space-x-3 md:space-x-5">
+              <button className="text-xs md:text-base bg-[#f9f9f9] text-black flex items-center justify-center py-2.5 px-6 rounded hover:bg-[#c6c6c6]">
+                <img
+                  src="/images/play-icon-black.svg"
+                  alt=""
+                  className="h-6 md:h-8"
+                />
+                <span className="uppercase font-medium tracking-wide">
+                  Play
+                </span>
+              </button>
+
+              <button
+                className="text-xs md:text-base bg-black/30 text-[#f9f9f9] border border-[#f9f9f9] flex items-center justify-center py-2.5 px-6 rounded hover:bg-[#c6c6c6]"
+                onClick={() => setShowPlayer(true)}
+              >
+                <img
+                  src="/images/play-icon-white.svg"
+                  alt=""
+                  className="h-6 md:h-8"
+                />
+                <span className="uppercase font-medium tracking-wide">
+                  Trailer
+                </span>
+              </button>
+              <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
+                <PlusIcon className="h-6" />
+              </div>
+
+              <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
+                <img src="/images/group-icon.svg" alt="" />
+              </div>
+            </div>
+
+            <p className="text-xs md:text-sm">
+              {result.release_date || result.first_air_date} •{" "}
+              {Math.floor(result.runtime / 60)}h {result.runtime % 60}m •{" "}
+              {result.genres.map((genre) => genre.name + " ")}{" "}
+            </p>
+            <h4 className="text-sm md:text-lg max-w-4xl">{result.overview}</h4>
+          </div>
+
+          {/* Bg Overlay */}
+          {showPlayer && (
+            <div className="absolute inset-0 bg-black opacity-50 h-full w-full z-50"></div>
+          )}
+          <div
+            className={`absolute top-3 inset-x-[7%] md:inset-x-[13%] rounded overflow-hidden transition duration-1000 ${
+              showPlayer ? "opacity-100 z-50" : "opacity-0"
+            }`}
+          >
+            <div>
+              <span className="font-semibold">Play Trailer</span>
+            </div>
           </div>
         </section>
       )}
